af488b929c768c461f82ecc4c3788d1f307686ed
jeroenvanderzwam <jeroenvanderzwam@hotmail.com>
2020-07-28 10:51:28 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jeroen van der Zwam is erg erg stoer
+          Jeroen van der Zwam is cool
         </p>
         <a
           className="App-link"
